09a9d64adfbac140a330a22d940cc77200134ad5
Bartosz <bartosz.szarkowski@student.put.poznan.pl>
2022-04-30 12:25:58 +0200

    finish

--- .github/main.py
+++ .github/main.py
@@ -5,20 +5,31 @@ x = list(wejscie)
 
 x1 = x[0]
 x2 = x[1]
+x3 = x1 - x2
+
 tablica_wynikow1 = []
 tablica_wynikow2 = []
+tablica_wynikow3 = []
 
 while x1 >= 1:
     tablica_wynikow1.append(x1)
     x1 = x1 - 1
 
 while x2 >= 1:
-    tablica_wynikow2.append(x1)
+    tablica_wynikow2.append(x2)
     x2 = x2 - 1
 
-print(tablica_wynikow1)
-print(tablica_wynikow2)
+while x3 >= 1:
+    tablica_wynikow3.append(x3)
+    x3 = x3 - 1
 
-mnozenie1 = math.prod(tablica_wynikow1)
-mnozenie2 = math.prod(tablica_wynikow2)
-print(mnozenie1, mnozenie2)
+#print(tablica_wynikow1)
+#print(tablica_wynikow2)
+#print(tablica_wynikow3)
+
+n_silnia = math.prod(tablica_wynikow1)
+k_silnia = math.prod(tablica_wynikow2)
+n_k_silnia = math.prod(tablica_wynikow3)
+
+symbol_newtona = n_silnia/(k_silnia*n_k_silnia)
+print(int(symbol_newtona))
